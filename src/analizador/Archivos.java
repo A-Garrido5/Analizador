92f775055803a19e1ac09f7e086b71f5c8ca4a9b
A-Garrido5 <adrian.garrido@usach.cl>
2015-07-13 01:58:48 -0400

    archivos

---
+++ src/analizador/Archivos.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analizador;
+
+/**
+ *
+ * @author Adrián
+ */
+public class Archivos {
+    
+    public void revisarPermisos(){
+        
+    }
+    
+}
