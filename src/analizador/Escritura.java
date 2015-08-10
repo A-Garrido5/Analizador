d75464c0cb241beaf0107c25055c9618feabdf6e
A-Garrido5 <adrian.garrido@usach.cl>
2015-08-10 02:25:59 -0400

    Intentando escribir en archivo final

---
+++ src/analizador/Escritura.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package analizador;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author Adrián
+ */
+public class Escritura {
+    
+    public Escritura(){
+        
+        FileWriter fichero = null;
+        PrintWriter pw = null;
+        try
+        {
+            fichero = new FileWriter("C:\\Users\\Adrián\\Documents\\NetBeansProjects\\Analizador");
+            pw = new PrintWriter(fichero);
+ 
+            for (int i = 0; i < 10; i++)
+                pw.println("Linea " + i);
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+           try {
+           // Nuevamente aprovechamos el finally para 
+           // asegurarnos que se cierra el fichero.
+           if (null != fichero)
+              fichero.close();
+           } catch (Exception e2) {
+              e2.printStackTrace();
+           }
+        }
+        
+    }
+    
+}
